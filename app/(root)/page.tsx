2d357651ba46418445ab88c0619e605aa5cbe57a
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-09-24 00:38:18 +0800

    fix(styling): fix some styling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -20,7 +20,7 @@ export default async function Home({
       <section className="pink_container">
         <h1 className="heading">Pitch Your Startup, <br /> Connext With Entrepreneurs</h1>
 
-        <p className="sub-heading max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
 
         <SearchForm query={query} />
       </section>
