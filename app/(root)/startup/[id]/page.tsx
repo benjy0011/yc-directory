4402684d08cf39a9ab3d2bb95615b42300efd141
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-09-25 00:29:50 +0800

    feat: Added Playlist - Editor Choice

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { Author, Startup } from "@/sanity/types";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,6 +10,7 @@ import markdownit from 'markdown-it'
 import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 const md = markdownit();
 
 export const experimental_ppr = true;
@@ -25,6 +26,9 @@ export default async function Page({
   const id = (await params).id;
 
   const post: Post  = await client.fetch(STARTUP_BY_ID_QUERY, { id })
+
+  const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: 'editor-picks' });
+  console.log(editorPosts)
 
   if(!post) return notFound();
 
@@ -90,8 +94,17 @@ export default async function Page({
 
         <hr className="divider" />
 
-        {/* TODO: EDITOR SELECETED STARTUPS */}
-        
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map(( post: StartupTypeCard, i: number ) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
 
         {/* Dynamic parts -- View is async */}
         <Suspense fallback={<Skeleton className="view-skeleton" />}>
